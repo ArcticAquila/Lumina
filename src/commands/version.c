e2631be38846587b99e8624bb9039f4d26d96c25
ArcticAquila <franklinazriel6942@outlook.com>
2023-10-02 00:09:55 +0700

    Create version.c
    NEW:
    - Adding version command (version variable not changed for yet)

---
+++ src/commands/version.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Define the package manager version
+
+//DISABLED FOR NOW
+//define PACKAGE_MANAGER_VERSION "v1.0.0"
+
+#define PACKAGE_MANAGER_VERSION "vBASE CODE DEVELOPMENT PROGRESS"
+// Function to print the version
+void printVersion() {
+    printf("Lumina Package Version: %s\n", PACKAGE_MANAGER_VERSION);
+}
